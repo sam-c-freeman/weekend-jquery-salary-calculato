a257380a586df26939cde3efe7fd8c8318b71c55
Sam Freeman <samcfreeman888@gmail.com>
2022-08-26 16:14:43 -0500

    tested employee array and loop

--- assignment/client.js
+++ assignment/client.js
@@ -1,11 +1,37 @@
 //add test employees here
 //add array here
 
-const employeeOne = {firstName: 'Charlie', type: 'dog', age: 6};
+const employeeOne = {
+    firstName: 'Sam', 
+    lastName: 'Freeman', 
+    idNumber: '9462', 
+    jobTitle: 'Cool Guy', 
+    annualSalary: '50000',
+};
+const employeeTwo = {
+    firstName: 'Sascha', 
+    lastName: 'Kala', 
+    idNumber: '2124', 
+    jobTitle: 'Software Engineer', 
+    annualSalary: '100000'
+};
 
+const employeeThree = {
+    firstName: 'Ro', 
+    lastName: 'McDonald', 
+    idNumber: '6666', 
+    jobTitle: 'Pet Wrangler', 
+    annualSalary: '75000'
+};
+
+
+const employees = [employeeOne, employeeTwo, employeeThree];
 
 $(document).ready(readyNow);
 function readyNow() {
-    
+    ()
   }
 
+for (let employee of employees){
+    console.log(employee);
+}
