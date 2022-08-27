b88229ccb4724710a58d6f4ebf4bc2973e322054
Sam Freeman <samcfreeman888@gmail.com>
2022-08-27 12:01:31 -0500

    added code for calculating monthly expenses

--- assignment/client.js
+++ assignment/client.js
@@ -1,31 +1,33 @@
 //add test employees here
 //add array here
 
-const employeeOne = {
-    firstName: 'Sam', 
-    lastName: 'Freeman', 
-    idNumber: '9462', 
-    jobTitle: 'Cool Guy', 
-    annualSalary: '50000',
-};
-const employeeTwo = {
-    firstName: 'Sascha', 
-    lastName: 'Kala', 
-    idNumber: '2124', 
-    jobTitle: 'Software Engineer', 
-    annualSalary: '100000'
-};
+// const employeeOne = {
+//     firstName: 'Sam', 
+//     lastName: 'Freeman', 
+//     idNumber: '9462', 
+//     jobTitle: 'Cool Guy', 
+//     annualSalary: '50000',
+// };
+// const employeeTwo = {
+//     firstName: 'Sascha', 
+//     lastName: 'Kala', 
+//     idNumber: '2124', 
+//     jobTitle: 'Software Engineer', 
+//     annualSalary: '100000'
+// };
 
-const employeeThree = {
-    firstName: 'Ro', 
-    lastName: 'McDonald', 
-    idNumber: '6666', 
-    jobTitle: 'Pet Wrangler', 
-    annualSalary: '75000'
-};
+// const employeeThree = {
+//     firstName: 'Ro', 
+//     lastName: 'McDonald', 
+//     idNumber: '6666', 
+//     jobTitle: 'Pet Wrangler', 
+//     annualSalary: '75000'
+// };
 
 
-const employees = [employeeOne, employeeTwo, employeeThree];
+// const employees = [employeeOne, employeeTwo, employeeThree];
+
+const employees = [];
 
 $(document).ready(readyNow);
 function readyNow() {
@@ -33,18 +35,19 @@ function readyNow() {
   }
 
 function collectEmployeeData (){
+    let monthlySalary = Math.round(($('#annual-salary').val()/12) * 100) / 100
     let newEmployee = {
         firstName: $('#first-name').val(),
         lastName: $('#last-name').val(),
         idNumber: $('#id-number').val(),
         jobTitle: $('#job-title').val(),
         annualSalary: $('#annual-salary').val(),
+        monthlySalary: Number(monthlySalary),
     }
     employees.push(newEmployee);
     emptyEmployeeInputs();
     addEmployeeToDom();
 }
-
 function emptyEmployeeInputs(){
     $('#first-name').val('');
     $('#last-name').val('');
